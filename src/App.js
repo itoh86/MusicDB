4270b94022b103c27e210e30e8e081ed5f30c034
itoh <itoh86@gmail.com>
2021-08-20 09:02:46 +0200

    30. Añadiendo proyecto de Firebase a nuestra App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import firebase from "./Utils/Firebase";
+import "firebase/auth";
 
 function App() {
+  firebase.auth().onAuthStateChanged(currentUser => {
+    console.log(currentUser ? "Estamos Logeados" : "No Estamos Logeados");
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>App Electron + React</h1>
     </div>
   );
 }
---
+++ src/Utils/Firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDZOBG6s7pXHQN61RacA334cELTNH3zhW4",
+    authDomain: "music-33566.firebaseapp.com",
+    projectId: "music-33566",
+    storageBucket: "music-33566.appspot.com",
+    messagingSenderId: "441761674250",
+    appId: "1:441761674250:web:401971367840cdf2775bf3"
+  };
+
+  export default firebase.initializeApp(firebaseConfig);
